d9c01e9070c5e5cf643262f23291cef2ff21c651
Emanuele Gissi <emanuele.gissi@gmail.com>
2020-06-09 17:51:44 +0200

    Bingeom routines

--- utils.py
+++ utils.py
@@ -90,6 +90,67 @@ def write_image(
     feedback.pushInfo(f"Texture saved, {wpix}x{hpix} pixels.")
 
 
+# The FDS bingeom file is written from Fortran90 like this:
+#      WRITE(731) INTEGER_ONE
+#      WRITE(731) N_VERTS,N_FACES,N_SURF_ID,N_VOLUS
+#      WRITE(731) VERTS(1:3*N_VERTS)
+#      WRITE(731) FACES(1:3*N_FACES)
+#      WRITE(731) SURFS(1:N_FACES)
+#      WRITE(731) VOLUS(1:4*N_VOLUS)
+
+import struct
+import numpy as np
+
+
+def _write_record(f, data):
+    """!
+    Write a record to a binary unformatted sequential Fortran90 file.
+    @param f: open Python file object in 'wb' mode.
+    @param data: np.array() of data.
+    """
+    # Calc start and end record tag
+    tag = len(data) * data.dtype.itemsize
+    # print(f"Write: record tag: {tag} dlen: {len(data)}\ndata: {data}")  # TODO log debug
+    # Write start tag, data, and end tag
+    f.write(struct.pack("i", tag))
+    data.tofile(f)
+    f.write(struct.pack("i", tag))
+
+
+def write_bingeom(
+    geom_type, n_surf_id, fds_verts, fds_faces, fds_surfs, fds_volus, filepath
+):
+    """!
+    Write FDS bingeom file.
+    @param geom_type: GEOM type (eg. 1 is manifold, 2 is terrain)
+    @param n_surf_id: number of referred boundary conditions
+    @param fds_verts: vertices coordinates in FDS flat format, eg. (x0, y0, z0, x1, y1, ...)
+    @param fds_faces: faces connectivity in FDS flat format, eg. (i0, j0, k0, i1, ...)
+    @param fds_surfs: boundary condition indexes, eg. (i0, i1, ...)
+    @param fds_volus: volumes connectivity in FDS flat format, eg. (i0, j0, k0, w0, i1, ...)
+    @param filepath: destination filepath
+    """
+
+    with open(filepath, "wb") as f:
+        _write_record(f, np.array((geom_type,), dtype="int32"))  # was 1 only
+        _write_record(
+            f,
+            np.array(
+                (
+                    len(fds_verts) // 3,
+                    len(fds_faces) // 3,
+                    n_surf_id,
+                    len(fds_volus) // 4,
+                ),
+                dtype="int32",
+            ),
+        )
+        _write_record(f, np.array(fds_verts, dtype="float64"))
+        _write_record(f, np.array(fds_faces, dtype="int32"))
+        _write_record(f, np.array(fds_surfs, dtype="int32"))
+        _write_record(f, np.array(fds_volus, dtype="int32"))
+
+
 # Geographic operations
 
 
