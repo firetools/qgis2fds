94f43bf56dfc64dfc9b615f8b2f7b1bf48358b24
Emanuele Gissi <emanuele.gissi@gmail.com>
2021-08-20 19:23:38 +0200

    Start work on new landuse

--- landuse.py
+++ landuse.py
@@ -2,33 +2,44 @@
 
 """qgis2fds"""
 
-__author__ = "Emanuele Gissi, Ruggero Poletto"
+__author__ = "Emanuele Gissi"
 __date__ = "2020-05-04"
 __copyright__ = "(C) 2020 by Emanuele Gissi"
 __revision__ = "$Format:%H$"  # replaced with git SHA1
 
 from dataclasses import dataclass
+from typing import Collection
 
 # FIXME not used yet
+
+# Classes
 
 
 @dataclass
 class DataColl:
-    coll = dict()  # redefine locally
+    collection = list()  # named collection of instances, redefine locally
     hid: str
 
     def __post_init__(self):
-        # Add to collection
-        if self.hid in self.coll:
-            raise IndexError(f"Item <{self.hid}> already present in collection {self}")
-        self.coll[self.hid] = self
+        if self.get_by_hid(self.hid):
+            raise IndexError(f"Item <{self.hid}> already defined")
+        self.coll[self.hid] = self  # add to named collection
+
+    @classmethod
+    def get_by_hid(cls, hid):
+        for c in cls.coll:
+            if hid == c.hid:
+                return c
 
 
 @dataclass
 class LanduseType(DataColl):
     coll = dict()  # local dict
     desc: str
-    surfs: str
+    surfs: tuple
+
+    def to_fds(self):
+        return "\n".join(self.surfs)
 
 
 @dataclass
@@ -38,43 +49,49 @@ class LanduseChoice(DataColl):
     choice: dict
     default: int = 0
 
+    def to_fds(self, value):
+        try:
+            return self.choice[value]
+        except KeyError:
+            raise KeyError(
+                f"Landuse value <{value}> has no reference to SURF in <{self.landuse_type.hid}> landuse type"
+            )
+
+
+# Landuse types and choices
+
 
 LanduseType(
     hid="Unspecified",
     desc="Unspecified",
-    surfs="""
-&SURF ID='NA' RGB=255,255,255 /
-""",
+    surfs=("&SURF ID='NA' RGB=255,255,255 /",),
 )
 
 LanduseType(
-    hid="Landfire F13",
+    hid="Landfire.gov F13",
     desc="Landfire.gov 13 Anderson Fire Behavior Fuel Models",
-    surfs="""
-&SURF ID='A01' RGB=255,254,212 VEG_LSET_FUEL_INDEX= 1 /
-&SURF ID='A02' RGB=255,253,102 VEG_LSET_FUEL_INDEX= 2 /
-&SURF ID='A03' RGB=236,212, 99 VEG_LSET_FUEL_INDEX= 3 /
-&SURF ID='A04' RGB=254,193,119 VEG_LSET_FUEL_INDEX= 4 /
-&SURF ID='A05' RGB=249,197, 92 VEG_LSET_FUEL_INDEX= 5 /
-&SURF ID='A06' RGB=217,196,152 VEG_LSET_FUEL_INDEX= 6 /
-&SURF ID='A07' RGB=170,155,127 VEG_LSET_FUEL_INDEX= 7 /
-&SURF ID='A08' RGB=229,253,214 VEG_LSET_FUEL_INDEX= 8 /
-&SURF ID='A09' RGB=162,191, 90 VEG_LSET_FUEL_INDEX= 9 /
-&SURF ID='A10' RGB=114,154, 85 VEG_LSET_FUEL_INDEX=10 /
-&SURF ID='A11' RGB=235,212,253 VEG_LSET_FUEL_INDEX=11 /
-&SURF ID='A12' RGB=163,177,243 VEG_LSET_FUEL_INDEX=12 /
-&SURF ID='A13' RGB=  0,  0,  0 VEG_LSET_FUEL_INDEX=13 /
-&SURF ID='Urban' RGB=186,119, 80 /
-&SURF ID='Snow-Ice' RGB=234,234,234 /
-&SURF ID='Agriculture' RGB=253,242,242 /
-&SURF ID='Water' RGB=137,183,221 /
-&SURF ID='Barren' RGB=133,153,156 /
-&SURF ID='NA' RGB=255,255,255 /
-""",
+    surfs=(
+        "&SURF ID='A01' RGB=255,254,212 VEG_LSET_FUEL_INDEX= 1 /",
+        "&SURF ID='A02' RGB=255,253,102 VEG_LSET_FUEL_INDEX= 2 /",
+        "&SURF ID='A03' RGB=236,212, 99 VEG_LSET_FUEL_INDEX= 3 /",
+        "&SURF ID='A04' RGB=254,193,119 VEG_LSET_FUEL_INDEX= 4 /",
+        "&SURF ID='A05' RGB=249,197, 92 VEG_LSET_FUEL_INDEX= 5 /",
+        "&SURF ID='A06' RGB=217,196,152 VEG_LSET_FUEL_INDEX= 6 /",
+        "&SURF ID='A07' RGB=170,155,127 VEG_LSET_FUEL_INDEX= 7 /",
+        "&SURF ID='A08' RGB=229,253,214 VEG_LSET_FUEL_INDEX= 8 /",
+        "&SURF ID='A09' RGB=162,191, 90 VEG_LSET_FUEL_INDEX= 9 /",
+        "&SURF ID='A10' RGB=114,154, 85 VEG_LSET_FUEL_INDEX=10 /",
+        "&SURF ID='A11' RGB=235,212,253 VEG_LSET_FUEL_INDEX=11 /",
+        "&SURF ID='A12' RGB=163,177,243 VEG_LSET_FUEL_INDEX=12 /",
+        "&SURF ID='A13' RGB=  0,  0,  0 VEG_LSET_FUEL_INDEX=13 /",
+        "&SURF ID='Urban' RGB=186,119, 80 /",
+        "&SURF ID='Snow-Ice' RGB=234,234,234 /",
+        "&SURF ID='Agriculture' RGB=253,242,242 /",
+        "&SURF ID='Water' RGB=137,183,221 /",
+        "&SURF ID='Barren' RGB=133,153,156 /",
+        "&SURF ID='NA' RGB=255,255,255 /",
+    ),
 )
-
-# print(LanduseType.coll["Landfire F13"])
-# print(LanduseType.coll)
 
 
 LanduseChoice(
@@ -84,7 +101,7 @@ LanduseChoice(
 )
 
 LanduseChoice(
-    hid="Landfire F13",
+    hid="Landfire.gov F13",
     landuse_type=LanduseType.coll["Landfire F13"],
     choice={
         0: 19,
@@ -124,5 +141,3 @@ LanduseChoice(
         7: 1,
     },
 )
-
-print(LanduseChoice.coll["Unspecified"])
