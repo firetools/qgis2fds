799db73afa3a15f5cc326b90b9c43ea429140c36
johodges <johodges@vt.edu>
2023-08-02 10:01:12 -0400

    Added golden gate remote case.

--- verification/headless_verify.sh
+++ verification/headless_verify.sh
@@ -5,3 +5,8 @@ cd ../..
 cd tests/test_golden_gate_local
 bash test_golden_gate_local_geom.sh
 bash test_golden_gate_local_obst.sh
+cd ../..
+cd tests/test_golden_gate_remote
+bash test_golden_gate_remote_geom.sh
+bash test_golden_gate_remote_obst.sh
+cd ../..
