27d4bf64dfa9325a8c2bedd6c6ba873b9c9575ef
johodges <johodges@vt.edu>
2023-08-02 09:40:53 -0400

    add golden gate local verification to actions

--- verification/headless_verify.sh
+++ verification/headless_verify.sh
@@ -2,3 +2,6 @@ cd tests/test_cern_meyrin
 bash test_cern_meyrin_geom.sh
 bash test_cern_meyrin_obst.sh
 cd ../..
+cd tests/test_golden_gate_local
+bash test_golden_gate_local_geom.sh
+bash test_golden_gate_local_obst.sh
