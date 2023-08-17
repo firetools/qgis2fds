53bc0ec96e50503ed48d5ff17f117cb010801744
johodges <johodges@vt.edu>
2023-08-17 07:50:41 -0400

    add chimney_tops_2 verification case

--- verification/evaluate.sh
+++ verification/evaluate.sh
@@ -13,3 +13,6 @@ fds golden_gate_remote_geom.fds
 fds golden_gate_remote_obst.fds
 smokeview golden_gate_remote_geom
 smokeview golden_gate_remote_obst
+cd ../../test_chimney_tops_2/output
+fds Chimney_Tops_2.fds
+smokeview Chimney_Tops_2
